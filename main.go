ce3e817b50aedce877fd499574af14df47c1cb6d
Sergeus27 <7vgy@mail.ru>
2026-05-19 07:58:49 +0300

    made sync

--- main.go
+++ main.go
@@ -2,8 +2,30 @@ package main
 
 import (
 	"fmt"
+	"sync"
 )
 
+func foo(wg *sync.WaitGroup) {
+	defer wg.Done()
+	fmt.Println("hello from foo")
+}
+
 func main() {
-	fmt.Println("hello gorutine")
+	wg := &sync.WaitGroup{}
+
+	wg.Add(1)
+	go func() {
+		defer wg.Done()
+
+		fmt.Println("hello from gorutine")
+	}() //анонимная функция внутри которой все будет запущено конкурентно
+	//анонимные функции?
+	//конкурентные функции?
+	//горутина это функция которая может запускаться конкурентно и работать конкурентно вместе с другими функциями
+
+	wg.Add(1)
+	go foo(wg)
+
+	wg.Wait() //ждет пока не отработают все Done
+	fmt.Println("hello from main")
 }
